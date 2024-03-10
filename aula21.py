ed4f5197e5ccf1435914987369f61e097b312911
Matheus Mesquita Yukishita <161179335+math-ghub@users.noreply.github.com>
2024-03-10 16:39:15 -0400

    Update aula21.py

--- aula21.py
+++ aula21.py
@@ -8,16 +8,31 @@
 
 # print(f"Paulo tem o seu direito a voto {voto(nascimento)[0]}, pois tem {voto(nascimento)[1]} anos.")
 
-def fatorial(n, show=False):
-    fatList = ""
-    result = val = n
-    while val > 1:
-        fatList += str(val) + " x "
-        result *= val - 1
-        val -= 1
-    fatList += "1 = " if len(fatList) > 2 and fatList[-2] == "x" else ""
-    if show:
-        return fatList + str(result)
-    return result
+# def fatorial(n, show=False):
+#     fatList = ""
+#     result = val = n
+#     while val > 1:
+#         fatList += str(val) + " x "
+#         result *= val - 1
+#         val -= 1
+#     fatList += "1 = " if len(fatList) > 2 and fatList[-2] == "x" else ""
+#     if show:
+#         return fatList + str(result)
+#     return result
 
-print(fatorial(100, True))
+# print(fatorial(8, True))
+
+# def ficha(jogador="<desconhecido>", gols=0):
+#     if jogador == "":
+#         jogador = "<desconhecido>"
+#     print(f"O jogador {jogador} fez {gols} gol(s) no campeonato.")
+
+# ficha("",5)
+
+# def leiaInt(n):
+#     if not n.isnumeric():
+#         n = leiaInt(input("Digite um número inteiro: "))
+#     else:
+#         print(f"Você acabou de digitar o número {n}")
+
+# leiaInt('a')
